17f2ad79d3db050ca90ffb668a72abcc10d83122
real7alireza <real7alireza@gmail.com>
2021-01-29 21:14:16 +0330

    redirecting with history prop

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -25,14 +25,15 @@ class NewPost extends Component {
             .then(response => {
                 console.log(response)
             })
-        this.setState({ submitted: true })
+        this.props.history.push('/posts')
+        // this.setState({ submitted: true })
     }
 
 
     render() {
         return (
             <div className="NewPost">
-                {this.state.submitted ? <Redirect to='/posts' /> : null}
+                {/* {this.state.submitted ? <Redirect to='/posts' /> : null} */}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
